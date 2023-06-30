a97070f357b7682a0d93fef1b5f453bcd39bbae8
nodebe <nodywelete3@gmail.com>
2023-06-30 15:43:04 +0100

    Created the Portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        textGrey: '#666666',
+        heroSolid: '#42446E',
+        heroGradientBegin: '#13B0F5',
+        heroGradientEnd: '#E70FAA',
+        pillButtonBackground: '#D7FFE0',
+        pillButtonText: '#018C0F',
+        lightText: '#A7A7A7',
+        contactText: '#1E0E62',
+      },
+    },
+  },
+  plugins: [],
+}
+
